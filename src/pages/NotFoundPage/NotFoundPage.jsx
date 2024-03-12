9232abcb173bf20bbe21e33021b78868ed03a6fd
OlVin69 <aviniichuk@gmail.com>
2024-03-12 09:10:00 +0200

    add all files

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import{Link} from "react-router-dom";
+
+export default function NotFoundPage () {
+    return (
+        <>
+          <Link to="/">Home</Link>
+          <p>Anything Is Not Found</p>
+        </>
+    
+    )
+}
